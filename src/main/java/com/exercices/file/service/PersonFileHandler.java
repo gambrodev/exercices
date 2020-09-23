7307246b5b23e1cd316ff491f2f5863684049698
DanyBorle <71042963+DanyBorle@users.noreply.github.com>
2020-09-23 10:54:05 +0200

    avanzamento pratica

--- src/main/java/com/exercices/file/service/PersonFileHandler.java
+++ src/main/java/com/exercices/file/service/PersonFileHandler.java
@@ -2,9 +2,11 @@ package com.exercices.file.service;
 import com.exercices.file.pojo.Person;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,23 +23,17 @@ public class PersonFileHandler {
     public List<String> readFromFile() {
         //googlelare Read text line by line java MKyong (Buffered Reader)
         //leggi dal file e metti ogni riga dentro una lista
-        StringBuilder DataArray = new StringBuilder();
-
-        try (BufferedReader br = person.newBufferedReader(Paths.get("Person.txt"))) {
-
-            // read line by line
-            String line;
-            while ((line = br.readLine()) != null) {
-                DataArray.append(line).append("\n");
-            }
-
-        } catch (IOException e) {
-            System.err.format("IOException: %s%n", e);
+        List<String> datalist = new ArrayList<>();
+        Files personFile = new Files ("C:\\Users\\PCasa\\IdeaProjects\\exercises\\Resources\\Person.txt");
+        BufferedReader reader = new BufferedReader(new FileReader(personFile));
+        String readLine = String ("");
+        while ((readLine = reader.readLine()) != null) {
+            datalist add readLine;
+        return datalist;
         }
-        return null;
+
     }
 
-    //dopo read volendo write
 
 
 }
